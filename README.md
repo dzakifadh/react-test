6b2a4adb86837efce114350daa5b4c0be701f79b
dzakifadh <fadh.dzaki@gmail.com>
2022-02-16 23:43:44 +0700

    Upload initial test

--- README.md
+++ README.md
@@ -1,70 +1,21 @@
-# Getting Started with Create React App
+# React Test
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Scope
 
-## Available Scripts
+Create a simple/small image search engine connected to the pixabay api in ReactJS with this Mockup ( it’s a mockup, feel free to do your own UX/UI ):
 
-In the project directory, you can run:
+> We will need to be able to search for a color/ flower and you will retrieve data from pixabay and display it
 
-### `npm start`
+Rules
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+- [x] Use at least once await and async functions (to get data from API)
+- [x] Use UseEffect Hook
+- [x] Use UseState Hook
+- [x] Use Javascript ( no typescript )
+- [x] Use Tailwind
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## How to release
 
-### `npm test`
+Please create a public github project with this small React JS Project and share the link with me !
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+## Please let me know if there any better approaches for this case, thanks
--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, Fragment } from "react"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const KEY_API = "25721109-43fefb31d736bd126016752a5"
+
+	const [collection, getCollection] = useState([])
+	const [detail, getDetail] = useState([])
+	const [type, getType] = useState()
+	const [isModalShow, setIsModalShow] = useState(false)
+	const [perPage, setPerPage] = useState(20)
+
+	const setModal = (id) => {
+		getDetailData(id)
+		setIsModalShow(!isModalShow)
+	}
+
+	useEffect(() => {
+		const getAllData = async () => {
+			const allPixabyDataResultData = await fetch(`https://pixabay.com/api/?key=${KEY_API}&q=${encodeURIComponent(type || "yellow flower")}&image_type=photo&pretty=true&per_page=${perPage}`)
+			const allPixabyDataResult = await allPixabyDataResultData.json()
+			getCollection(allPixabyDataResult.hits)
+		}
+		getAllData()
+	}, [type, perPage])
+
+	const getDetailData = async (id) => {
+		const detailPixabyData = await fetch(`https://pixabay.com/api/?key=${KEY_API}&q=${encodeURIComponent(type || "yellow flower")}&image_type=photo&pretty=true&id=${id}`)
+		const detailPixabyDataResult = await detailPixabyData.json()
+		getDetail(detailPixabyDataResult.hits)
+	}
+
+	useEffect(() => {}, [isModalShow])
+
+	return (
+		<div className="relative">
+			<div className="flex flex-col items-center justify-center mb-10">
+				<div className="flex flex-col items-center my-4">
+					<label className="mb-4 text-3xl font-semibold">PIXABY</label>
+					<input type="text" value={type} className="px-4 py-2 border-2 rounded" placeholder="Search Here ..." onChange={(e) => getType(e.target.value)} />
+				</div>
+				<div className="flex flex-wrap justify-center gap-4 m-10">
+					{collection.length > 0
+						? collection.map((item, index) => (
+								<div key={index} className="overflow-hidden hover:cursor-pointer group" onClick={() => setModal(item.id)}>
+									<img src={item.webformatURL} alt={`Thumbnail ${item.id}`} className="object-cover w-64 h-48 duration-200 group-hover:scale-110" />
+								</div>
+						  ))
+						: " So sory, we cannot find what did you search :("}
+				</div>
+				<button type="button" className="px-8 py-3 text-white rounded bg-slate-700" onClick={() => setPerPage((prev) => prev + 10)}>
+					Load More
+				</button>
+			</div>
+
+			<div className={`fixed inset-0 flex items-center justify-center ${isModalShow ? "opacity-1 z-10 visible" : "opacity-0 -z-10 invisible"} bg-opacity-50 bg-slate-900 duration-200`}>
+				<div className={`relative w-2/4 p-6 bg-white rounded text-slate-900 duration-500 ${isModalShow ? "translate-y-0 visible" : "translate-y-20 invisible"}`}>
+					<span className="absolute top-6 right-6 hover:cursor-pointer" onClick={() => setIsModalShow(false)}>
+						<svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</span>
+					<h2 className="mb-6 text-2xl font-medium">Detail data</h2>
+					<ul className="grid grid-cols-3 gap-4">
+						{detail.map((item, index) => (
+							<Fragment key={index}>
+								<li>
+									<p className="mb-4 font-medium">Author</p>
+									<div className="flex items-center gap-4">
+										{item.userImageURL !== "" ? (
+											<img src={item.userImageURL} alt={`Author thumbnail, ${item.userImageURL}`} className="w-12 h-12 rounded-full" />
+										) : (
+											<img src={`https://ui-avatars.com/api/?name=${encodeURIComponent(item.user)}`} alt={`Author thumbnail, ${item.userImageURL}`} className="w-12 h-12 rounded-full" />
+										)}
+										<span>{item.user}</span>
+									</div>
+								</li>
+								<li>
+									<p className="font-medium">Views</p>
+									<span>{item.views}</span>
+								</li>
+								<li>
+									<p className="font-medium">Likes</p>
+									<span>{item.likes}</span>
+								</li>
+								<li>
+									<p className="font-medium">Downloads</p>
+									<span>{item.downloads}</span>
+								</li>
+								<li>
+									<p className="font-medium">Types</p>
+									<span>{item.type}</span>
+								</li>
+								<li>
+									<p className="font-medium">Tags</p>
+									<span>{item.tags}</span>
+								</li>
+							</Fragment>
+						))}
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
