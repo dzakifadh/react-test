ea1fd2baafd2b8c8462c959bb3b6d7ed7eac28f5
dzakifadh <fadh.dzaki@gmail.com>
2022-02-17 07:31:31 +0700

    Update responsive

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
 					<label className="mb-4 text-3xl font-semibold">PIXABY</label>
 					<input type="text" value={type} className="px-4 py-2 border-2 rounded" placeholder="Search Here ..." onChange={(e) => getType(e.target.value)} />
 				</div>
-				<div className="grid flex-wrap justify-center grid-cols-2 gap-2 m-4 sm:gap-4 sm:m-10 sm:flex">
+				<div className="grid flex-wrap justify-center grid-cols-2 gap-2 mx-2 my-4 sm:m-4 sm:gap-4 md:m-10 sm:flex">
 					{collection.length > 0 ? (
 						collection.map((item, index) => (
 							<div key={index} className="overflow-hidden hover:cursor-pointer group" onClick={() => setModal(item.id)}>
@@ -55,8 +55,8 @@ function App() {
 			</div>
 
 			<div className={`fixed inset-0 flex items-center justify-center ${isModalShow ? "opacity-1 z-10 visible" : "opacity-0 -z-10 invisible"} bg-opacity-50 bg-slate-900 duration-200`}>
-				<div className={`relative overflow-auto rounded-none h-5/6 w-11/12 md:w-3/4 p-6 bg-white text-slate-900 duration-500 ${isModalShow ? "translate-y-0 visible" : "translate-y-20 invisible"}`}>
-					<span className="absolute top-6 right-6 hover:cursor-pointer" onClick={() => setIsModalShow(false)}>
+				<div className={`relative overflow-auto rounded-none h-5/6 w-11/12 md:w-3/4 py-6 px-3 sm:p-6 bg-white text-slate-900 duration-500 ${isModalShow ? "translate-y-0 visible" : "translate-y-20 invisible"}`}>
+					<span className="absolute top-6 right-3 sm:right-6 hover:cursor-pointer" onClick={() => setIsModalShow(false)}>
 						<svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
 						</svg>
